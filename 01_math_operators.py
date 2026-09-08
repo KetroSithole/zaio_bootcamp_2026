cfb5ce8ae206fc64e45e5f4c9ab1668201d3ee49
greek-hacker <u18312889@tuks.co.za>
2026-09-08 17:51:20 +0200

    basic maths ops

---
+++ 01_math_operators.py
@@ -0,0 +1,12 @@
+# --- Basic math operators ---
+first_number = 10
+second_number = 3
+print(
+    first_number + second_number,
+    first_number - second_number,
+    first_number * second_number,
+    first_number / second_number,
+    first_number % second_number,
+    first_number ** second_number,
+    first_number // second_number,
+)
